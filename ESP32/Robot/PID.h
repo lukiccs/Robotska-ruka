9e242914c05804329dd63591aa576ce090d0cd3a
lukiccs <lukicslobodan26@gmail.com>
2025-07-14 01:39:05 +0200

    PID fajlovi

---
+++ ESP32/Robot/PID.h
@@ -0,0 +1,14 @@
+#ifndef PID_H
+#define PID_H
+
+#include <GLobalnePromenljive.h>
+#include <Arduino.h>
+#include "RadMotora.h"
+
+const int KP = 1; // Proporcionalna konstanta
+const int KI = 0; // Integralna konstanta
+const int KD = 0; // Derivativna konstanta
+
+
+
+#endif
